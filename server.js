880ade5c8845df4a5f2f594b3b54ef188f8e3030
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-16 13:10:23 -0500

    prac

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const express = require('express');
 const app = express();
 
 // sets up route handlers - this is how we respone to requests
-app.get('/', (request, response)=> {
+app.get('/cats', (req, res) => {
+    
 
    // send an http response using the express response object
    // using .send() method
-respond.send('Hello World');
+res.send(['mr.worms', 'garfield', 'sachi']);
 
 });
+app.get('/about', (req,res) => {
+
+res.send('<p>Everything about this website<p>')
+
+});
+
 //added arrow function
 app.listen(3000, () => {
 
-console.log('Expresis is listening for requests from brower on port 3000')
+console.log('Express is is listening for requests from brower on port 3000')
 
 });
