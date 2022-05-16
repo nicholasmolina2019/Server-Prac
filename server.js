f13b423ba976ed950f62b4cf5ad3318a601860bb
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-16 11:16:43 -0500

    using .send() method

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const express = require('express');
 
 // this activates the express framework
 const app = express();
+
+// sets up route handlers - this is how we respone to requests
+app.get('/', (request, response)=> {
+
+   // send an http response using the express response object
+   // using .send() method
+respond.send('Hello World');
+
+});
